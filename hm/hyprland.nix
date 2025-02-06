42076d8e63de376d55b7acf12f9f381978478ccf
Gorgeous-Patrick <baichuanli@yahoo.com>
2025-02-06 11:48:54 -0500

    feat: gui applications bundle

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -4,6 +4,7 @@
         ../bundles/cli-base.nix
         ../bundles/programming.nix
 	../bundles/db.nix
+	../bundles/gui.nix
         ../applications/nvim-nix/nvim.nix
 	../applications/hyprland/hyprland.nix
     ];
