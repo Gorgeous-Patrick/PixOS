ffa98eb1486068b232f513694267bd8bdf195117
Gorgeous-Patrick <baichuanli@yahoo.com>
2025-03-16 01:32:13 +0000

    feat: WSL release

---
+++ release/WSL/flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-wsl.url = "github:nix-community/nixos-wsl";
+    pixos-nixvim.url = "path:../../applications/nvim-nix";
+    nix-update.url = "path:../../applications/nix-update";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, nixos-wsl, pixos-nixvim, nix-update, home-manager }: 
+
+	let
+platform =  "x86_64-linux";
+homeconfig = import ../../hm/cli-work.nix;
+config = { config, lib, pkgs, ... }:
+
+{
+environment.systemPackages = [
+            pkgs.vim
+            pkgs.zsh
+            pkgs.nano
+            pkgs.wget
+            pkgs.curl
+            pixos-nixvim.packages."${platform}".default
+            nix-update.packages."${platform}".rebuild
+            nix-update.packages."${platform}".update
+          ];
+          programs.zsh.enable = true;
+          users.defaultUserShell = pkgs.zsh;
+};
+in
+{
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix nixos-wsl.nixosModules.wsl config 
+      home-manager.nixosModules.home-manager
+      
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users."patrickli" = homeconfig;
+          }
+      ];
+    };
+  };
+}
