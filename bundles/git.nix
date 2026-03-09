25ca9e4076f4c213a377361f75e188242f343d3d
Patrick Li <baichuanli@yahoo.com>
2026-03-09 18:41:07 -0400

    fix: use correct path

--- bundles/git.nix
+++ bundles/git.nix
@@ -29,8 +29,8 @@ in
           userEmail = "baichuanli@yahoo.com";
           extraConfig = {
             pull.rebase = true;
-            core.pager = "delta";
-            interactive.diffFilter = "delta --color-only";
+            core.pager = "${pkgs.delta}/bin/delta";
+            interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
             delta = {
               navigate = true;
               side-by-side = true;
