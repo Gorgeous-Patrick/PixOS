3509cde78015bfd6a988d4cb26484594c5110d3a
Patrick Li <baichuanli@yahoo.com>
2026-03-23 16:03:32 -0400

    feat: volume

--- bundles/hyprland.nix
+++ bundles/hyprland.nix
@@ -203,6 +203,16 @@ in
               "$mod SHIFT, C, centerwindow"
             ];
 
+            # Volume
+            bindel = [
+              ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+              ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+            ];
+            bindl = [
+              ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+              ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+            ];
+
             bindm = [
               "$mod, mouse:272, movewindow"
               "$mod, mouse:273, resizewindow"
