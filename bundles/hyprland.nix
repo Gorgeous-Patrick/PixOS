b1082f874164c7dfa0888c82cb7e666c1e81d1c0
Patrick Li <baichuanli@yahoo.com>
2026-06-18 15:43:54 -0400

    scrolling layout

--- bundles/hyprland.nix
+++ bundles/hyprland.nix
@@ -134,7 +134,12 @@ in
               gaps_in = 0;
               gaps_out = 5;
               border_size = 2;
-              layout = "master";
+              layout = "scrolling";
+            };
+
+            scrolling = {
+              fullscreen_on_one_column = true;
+              column_width = 0.6667;
             };
 
             decoration = {
@@ -156,9 +161,17 @@ in
               "$mod, Z, exec, wofi --show drun"
               "$mod, X, exec, firefox"
               "$mod, Space, togglefloating"
-              "$mod, P, layoutmsg, pseudo"
-              "$mod, J, layoutmsg, togglesplit"
               "$mod, S, exec, hyprshot -m region --clipboard-only"
+
+              # Scrolling layout
+              "$mod, comma, layoutmsg, move -col"
+              "$mod, period, layoutmsg, move +col"
+              "$mod SHIFT, comma, layoutmsg, swapcol l"
+              "$mod SHIFT, period, layoutmsg, swapcol r"
+              "$mod, bracketleft, layoutmsg, colresize -100"
+              "$mod, bracketright, layoutmsg, colresize +100"
+              "$mod, equal, layoutmsg, fit active"
+              "$mod SHIFT, equal, layoutmsg, fit visible"
 
               ", XF86MonBrightnessUp, exec, brightnessctl set +5%"
               ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
